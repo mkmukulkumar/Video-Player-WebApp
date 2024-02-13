520a495f25e96e02be5b91dc8c27fd55a4aa589a
mkmukulkumar <mukulkumar258@gmail.com>
2024-02-14 03:08:52 +0530

    playlist list

--- app/components/ListView.tsx
+++ app/components/ListView.tsx
@@ -12,20 +12,22 @@ interface ListViewProps {
   data: ListItem[];
 }
 
-export default function ListView({ data }:ListViewProps) {
+export default function ListView({data}:ListViewProps) {
     return (
-      <ul className="list-disc space-y-10">
+      <ul className="list-none space-y-5 mx-48">
         {data.map((item) => (
-          <li className="bg-violet-800 flex flex-row" key={item.title}>
-            <a className="bg-yellow-800 basis-1/4" href={item.sources[0]}>
-                <img src={`http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/${item.thumb}`} alt={item.title} />
-            </a> 
-            <div className="flex flex-col basis-3/4 justify-center mx-10">
-                <p className="text-xl">{item.title}</p>   
-                <p className='text-lg'>{item.subtitle}</p>
-                <p className='text-sm'>{item.description}</p>  
-            </div>      
-          </li>
+            <li className="bg-stone-950 p-1 hover:bg-stone-900 transform transition-transform ease-in-out duration-300 hover:scale-105" key={item.title}>
+                <a href={item.sources[0]} className="flex flex-row p-1">
+                <div className="basis-1/4">
+                    <img className="object-cover" src={`http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/${item.thumb}`} alt={item.title} />
+                </div> 
+                <div className="flex flex-col basis-3/4 justify-center mx-10">
+                    <p className="text-sm font-bold">{item.title}</p>   
+                    <p className='text-xs mt-1'>{item.subtitle}</p>
+                    <p className='text-xs font-thin mt-3'>{item.description}</p>  
+                </div>  
+                </a>    
+            </li>
         ))}
       </ul>
     );
