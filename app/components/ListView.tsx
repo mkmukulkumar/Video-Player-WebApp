81244394bd9ee5932d2ed4ba73d4f434ab9d5108
mkmukulkumar <mukulkumar258@gmail.com>
2024-02-14 23:53:23 +0530

    pause/play added

--- app/components/ListView.tsx
+++ app/components/ListView.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React, { useState } from 'react';
 import {Grid, List} from 'react-bootstrap-icons';
 interface ListItem{
@@ -33,17 +34,18 @@ export default function ListView({data}:ListViewProps) {
         </div>
         <ul className={`list-none grid gap-5 ${toggleViewMode?'grid-cols-4':'grid-cols-1'} mx-48`}>
           {data.map((item) => (
-              <li className="bg-stone-950 p-1  hover:bg-stone-900 transform transition-transform ease-in-out duration-300 hover:scale-105" key={item.title}>
-                    <a href={`/video?${item.sources[0]}`} className={`flex ${toggleViewMode?"flex-col":"flex-row"} p-1`}>
-                    <div className="basis-1/4">
-                        <img className="object-cover" src={`http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/${item.thumb}`} alt={item.title} />
-                    </div> 
-                    <div className={`flex flex-col basis-3/4 ${toggleViewMode?'m-1':'mx-5 my-6'}`}>
-                        <p className="text-sm font-bold">{item.title}</p>   
-                        <p className='text-xs mt-1'>{item.subtitle}</p>
-                        {toggleViewMode?'':<p className='text-xs font-thin mt-3'>{item.description}</p>}
-                    </div>  
-                    </a>   
+              <li className="bg-neutral-900 p-1  hover:bg-neutral-900 transform transition-transform ease-in-out duration-300 hover:scale-105" 
+                  key={item.title}>
+                    <Link href={{pathname:'/video', query:{url:item.sources[0]}}} className={`flex ${toggleViewMode?"flex-col":"flex-row"} p-1`}>
+                      <div className="basis-1/4">
+                          <img className="object-cover" src={`http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/${item.thumb}`} alt={item.title} />
+                      </div> 
+                      <div className={`flex flex-col basis-3/4 ${toggleViewMode?'m-1':'mx-5 my-6'}`}>
+                          <p className="text-sm font-bold">{item.title}</p>   
+                          <p className='text-xs mt-1'>{item.subtitle}</p>
+                          {toggleViewMode?'':<p className='text-xs font-thin mt-3'>{item.description}</p>}
+                      </div>  
+                    </Link>   
               </li>
           ))}
         </ul>
--- app/video/page.tsx
+++ app/video/page.tsx
@@ -1,3 +1,69 @@
+"use client"
+import { useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
+import { useState, useRef, useEffect} from 'react';
+import { PlayFill, ChevronDoubleRight, ChevronDoubleLeft, PauseFill, VolumeUpFill, Fullscreen, ArrowLeft, FullscreenExit} from 'react-bootstrap-icons';
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
-}
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const videoRef=useRef(null);
+  const url = searchParams.get('url');
+  if (url === null) {
+    router.push('/');
+    return null;
+  }
+  const [Play,setPlay]=useState(true);
+  const [fullscreen,setFullscreen]=useState(true);
+  const handleplay=()=>{
+    const video=videoRef.current;
+    if(video){
+      if(!Play)
+        video.play();
+      else
+        video.pause();
+      setPlay(!Play)
+    }
+    
+  }
+  const handlefullscreen=()=>{
+    setFullscreen(!fullscreen)
+  }
+
+  useEffect(() => {
+    const video=videoRef.current;
+    if(video){
+      if(Play)
+        video.play();
+      else
+        video.pause();
+    }
+  }, [Play]);
+  return (
+    <div className='flex flex-col bg-black justify-center'>
+      <video ref={videoRef} className="h-screen">
+        <source
+          src={url}
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>
+      <button className='text-2xl py-2 fixed top-8 left-48'onClick={()=>router.push('/')}>
+            <div className='flex items-center'>
+              <ArrowLeft/><text className='text-xl mx-4'>Title</text>
+            </div>
+      </button>
+      
+      <div className='fixed bottom-0 w-full mb-8'>
+        <div className='flex  justify-between mx-48'>
+          <div className='flex justify-between w-1/4'>
+            <button className='text-2xl'onClick={handleplay}><ChevronDoubleLeft/></button>
+            <button className='text-6xl' onClick={handleplay}>{Play?<PlayFill/>:<PauseFill/>}</button>
+            <button className='text-2xl' onClick={handleplay}><ChevronDoubleRight/></button>
+            <button className='text-2xl'onClick={handleplay}><VolumeUpFill/></button>
+          </div>
+            <button className='text-xl'onClick={handlefullscreen}>{fullscreen?<FullscreenExit/>:<Fullscreen/>}</button>
+        </div>
+      </div>  
+    </div>
+  );
+};
