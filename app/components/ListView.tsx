5a76ab932ffe66ab94c6b42e6f9e8a1299385045
mkmukulkumar <mukulkumar258@gmail.com>
2024-02-16 04:46:56 +0530

    controls functionality hide/show control seekbar added

--- app/components/ListView.tsx
+++ app/components/ListView.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React, { useState } from 'react';
-import {Grid, List} from 'react-bootstrap-icons';
+import {Grid, List, Option, ThreeDots, ThreeDotsVertical} from 'react-bootstrap-icons';
 interface ListItem{
     description: string;
     sources: string[];
@@ -33,19 +33,22 @@ export default function ListView({data}:ListViewProps) {
             <List/>
           </button> 
         </div>
-        <ul className={`list-none grid gap-5 ${toggleViewMode?'grid-cols-4':'grid-cols-1'} mx-48`}>
+        <ul className={`list-none grid ${toggleViewMode?'grid-cols-4 gap-y-8 gap-x-2':'grid-cols-1 gap-2'} mx-48`}>
           {data.map((item) => (
-              <li className="bg-neutral-900 p-1  hover:bg-neutral-900 transform transition-transform ease-in-out duration-300 hover:scale-105" 
+              <li className={`bg-neutral-900 rounded p-1 hover:bg-neutral-800 transform transition-transform ease-in-out duration-300 hover:z-10 hover:${toggleViewMode?'scale-125':'scale-105'} hover:drop-shadow-2xl`} 
                   key={item.title}>
                     <Link href={{pathname:'/video', query:{url:item.sources[0]}}} className={`flex ${toggleViewMode?"flex-col":"flex-row"} p-1`}>
                       <div className="basis-1/4">
-                          <img className="object-cover" src={`http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/${item.thumb}`} alt={item.title} />
+                          <img className="object-cover rounded-lg" src={`http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/${item.thumb}`} alt={item.title} />
                       </div> 
-                      <div className={`flex flex-col basis-3/4 ${toggleViewMode?'m-1':'mx-5 my-6'}`}>
-                          <p className="text-sm font-bold">{item.title}</p>   
-                          <p className='text-xs mt-1'>{item.subtitle}</p>
-                          {toggleViewMode?'':<p className='text-xs font-thin mt-3'>{item.description}</p>}
-                      </div>  
+                      <div className={`flex justify-between basis-3/4 ${toggleViewMode?'items-center':'item-start'}`}>
+                        <div className={`flex flex-col  ${toggleViewMode?'m-1':'mx-5 my-6'}`}>
+                            <p className="text-sm font-medium">{item.title}</p>   
+                            <p className='text-xs text-neutral-400'>{item.subtitle}</p>
+                            {toggleViewMode?'':<p className='text-xs  font-thin mt-3'>{item.description}</p>}
+                        </div>
+                        <p><ThreeDotsVertical/></p> 
+                      </div>
                     </Link>   
               </li>
           ))}
--- app/video/page.tsx
+++ app/video/page.tsx
@@ -16,6 +16,10 @@ export default function Page() {
   //refernce variable
   const videoRef=useRef(null);
   const videoplayerRef=useRef(null);
+  const timerRef = useRef(null);
+  const showtimeRef = useRef(null);
+  const centertimeRef = useRef(null);
+  
   
   //state variables
   const [Play,setPlay]=useState(false);
@@ -24,8 +28,11 @@ export default function Page() {
   const [Duration,setDuration]=useState("00:00:00");
   const [CurrentTime, setCurrentTime]=useState("00:00:00");
   const [PlaybackRate, setPlaybackRate]=useState(1);
-  const timerRef = useRef(null);
+  const [ControlsVisible,setControlsVisible]=useState(false);
+  const [CenterIcon,setCenterIcon]=useState(false);
+  const [CenterTag,setCenterTag]=useState(<PlayFill/>)
   const playbackOptions=[0.25,0.5,0.75,1,1.25,1.5,1.75,2]
+
 
   // start and stop timer of video
   const startTimer = () => {
@@ -39,7 +46,7 @@ export default function Page() {
           } else {
             var date_format=convertSeconds(Math.ceil(video.duration))
             setCurrentTime(date_format);
-            stopTimer();
+            setPlay(!Play)
           }
         }
       }, 1000);
@@ -74,12 +81,15 @@ export default function Page() {
     if (video) {
       if (!Play) {
         video.play();
+        setCenterTag(<PlayFill/>)
         startTimer();
       } else {
         video.pause();
+        setCenterTag(<PauseFill/>)
         stopTimer();
       }
     }
+
   }, [Play]);
 
   //to get duration of video on refreshing the page
@@ -106,11 +116,34 @@ export default function Page() {
     if(video){
       video.playbackRate=event.target.value;
     }
+    setCenterTag(<p className='text-5xl'>{event.target.value}x</p>)
     setPlaybackRate(event.target.value);
   }
-  
+
+  //to hide controls on no mouse movement
+  const showControls=()=>{
+    setControlsVisible(true);
+    clearTimeout(showtimeRef.current)
+    showtimeRef.current=setTimeout(() => {
+      setControlsVisible(false);
+    }, 5000);
+  }
+
+  //to show icons on the center of the page
+  useEffect(() => { 
+    setCenterIcon(true)
+    clearTimeout(centertimeRef.current)
+    centertimeRef.current=setTimeout(() => {
+      setCenterIcon(false);
+    }, 500)
+  }, [CenterTag]);
+
   return (
-    <div ref={videoplayerRef} className='flex flex-col bg-black justify-center h-screen'>
+    <div ref={videoplayerRef} 
+    className='flex flex-col bg-black justify-center h-screen'
+    onMouseMove={showControls}
+    onClick={showControls}
+    >
       <video muted={Mute} 
           ref={videoRef}
           className="h-full"
@@ -118,33 +151,49 @@ export default function Page() {
           onClick={handleplay}>
         <source src={url} type="video/mp4"/>
         Your browser does not support the video tag.
-      </video>
-      <button className='text-2xl py-2 fixed top-8 left-48'onClick={()=>{ router.push('/')}}>
-            <div className='flex items-center'>
-              <ArrowLeft/><p className='text-xl mx-4'>Title</p>
-            </div>
-      </button>
-      <div className='fixed bottom-0 w-full mb-8'>
-        {(CurrentTime==="NaN:NaN:NaN"||Duration==="NaN:NaN:NaN")?<p className='mx-48 mb-2'></p>:<p className='mx-48 mb-2'>{CurrentTime} / {Duration}</p>}
-        <div className='flex  justify-between mx-48'>
-          <div className='flex justify-between w-1/4'>
-            <button className='text-2xl'onClick={handleplay}><ChevronDoubleLeft/></button>
-            <button className='text-6xl' onClick={handleplay}>{Play?<PlayFill/>:<PauseFill/>}</button>
-            <button className='text-2xl' onClick={handleplay}><ChevronDoubleRight/></button>
-            <button className='text-2xl'onClick={()=>{setMute(!Mute)}}>{Mute?<VolumeMute/>:<VolumeUpFill/>}</button>
+      </video>      
+      <div className={`fixed bottom-0 w-full`}>
+        {/* black shadow on top controls */}
+        <div className={`fixed -top-24 w-screen bg-black h-48 blur-2xl bg-opacity-70  duration-700 ease-in-out ${ControlsVisible?"translate-y-0":"-translate-y-40"}`}></div>
+        <button className={`text-2xl w-full px-48 py-12 fixed top-0 left-0  duration-700 ease-in-out ${ControlsVisible?"translate-y-0":"-translate-y-40"}`} 
+              onClick={()=>{ router.push('/')}}>
+                <div className='flex items-center'>
+                  <ArrowLeft/><p className='text-xl mx-4'>Title</p>
+                </div>
+        </button>
+        {/* center icons */}
+        <div className={`text-8xl fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-scale duration-500 ${CenterIcon ? 'scale-100' : 'scale-0'}`} onClick={handleplay}>{CenterTag}</div>
+        {/* black shadow on controls */}
+        <div className={`fixed -bottom-24 w-screen bg-black h-48 blur-2xl bg-opacity-70  duration-700 ease-in-out ${ControlsVisible?"translate-y-0":"translate-y-40"}`}></div>
+        
+        <div className={`py-4 mx-48 duration-700 ease-in-out ${ControlsVisible?"translate-y-0":"translate-y-40"}`}>  
+          <div className="mb-2">
+            <input className='w-full h-1' type="range" />
+            <p>{(CurrentTime==="NaN:NaN:NaN"||Duration==="NaN:NaN:NaN")?'':`${CurrentTime} / ${Duration}`}</p>
           </div>
-          <div className='flex items-center'>
-            <div className='flex items-center'>
-              <select defaultValue={1} className='text-xs rounded outline-0 bg-transparent mx-14 py-1 cursor-pointer border ' onChange={handlePlaybackRate}>
-                  {playbackOptions.map((option) => (
-                    <option className='bg-neutral-800' key={option} value={option}>
-                      {option}x
-                    </option>
-                  ))}
-              </select>
-              <button className='text-xl'onClick={handlefullscreen}>{fullscreen?<FullscreenExit/>:<Fullscreen/>}</button> 
+          <div className='flex  justify-between'>
+            <div className='flex justify-between w-1/3'>
+              <button className='text-2xl'onClick={()=>{videoRef.current.currentTime=videoRef.current.currentTime-10; setCenterTag(<ChevronDoubleLeft className='text-6xl'/>)}}><ChevronDoubleLeft/></button>
+              <button className='text-6xl' onClick={handleplay}>{Play?<PlayFill/>:<PauseFill/>}</button>
+              <button className='text-2xl' onClick={()=>{videoRef.current.currentTime=videoRef.current.currentTime+10; setCenterTag(<ChevronDoubleRight className='text-6xl'/>)}}><ChevronDoubleRight/></button>
+              <div className='flex items-center'>
+                <button className='text-2xl'onClick={()=>{setMute(!Mute); Mute?setCenterTag(<VolumeUpFill/>):setCenterTag(<VolumeMute/>)}}>{Mute?<VolumeMute/>:<VolumeUpFill/>}</button>
+                <input className='ml-2 w-20 h-0.5' type="range" />
+              </div>  
             </div>
-          </div>
+            <div className='flex items-center'>
+              <div className='flex items-center'>
+                <select defaultValue={PlaybackRate} className='text-sm s outline-0 bg-transparent mx-14 py-1 cursor-pointer' onChange={handlePlaybackRate}>
+                    {playbackOptions.map((option) => (
+                      <option className='bg-neutral-800' key={option} value={option}>
+                        {option}x
+                      </option>
+                    ))}
+                </select>
+                <button className='text-xl'onClick={handlefullscreen}>{fullscreen?<FullscreenExit/>:<Fullscreen/>}</button> 
+              </div>
+            </div>
+          </div>  
         </div>
       </div>  
     </div>
