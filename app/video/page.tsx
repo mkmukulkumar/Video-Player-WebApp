7361fd37845d7de6b2c09e43cea5b3ccf79d5077
mkmukulkumar <mukulkumar258@gmail.com>
2024-02-16 21:23:49 +0530

    autoplay fix

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -1,9 +1,8 @@
 "use client"
 import { useRouter, useSearchParams } from 'next/navigation';
-import { useState, useRef, useEffect, JSXElementConstructor, PromiseLikeOfReactNode, ReactElement, ReactNode, SetStateAction} from 'react';
+import { useState, useRef, useEffect, JSXElementConstructor, PromiseLikeOfReactNode, ReactElement, ReactNode, SetStateAction, Suspense} from 'react';
 import convertSeconds from '../utilFunctions/convertSeconds';
 import { PlayFill, ChevronDoubleRight, ChevronDoubleLeft, PauseFill, VolumeUpFill, Fullscreen, ArrowLeft, FullscreenExit, VolumeMute} from 'react-bootstrap-icons';
-import { Suspense } from 'react';
 export default function Page() {
 
   // reference variables
@@ -42,7 +41,7 @@ export default function Page() {
           } else {
             var date_format=convertSeconds(Math.ceil(video.duration))
             setCurrentTime(date_format);
-            setPlay(!Play)
+            setPlay(false)
           }
         }
       }, 1000); 
@@ -81,7 +80,8 @@ export default function Page() {
         video.play();
         setCenterTag(<PlayFill/>)
         startTimer();
-      } else {
+      } 
+      else {
         video.pause();
         setCenterTag(<PauseFill/>)
         stopTimer();
@@ -195,72 +195,77 @@ export default function Page() {
   }
 
   return (
-        <div ref={videoplayerRef} 
+      
+        <div  ref={videoplayerRef} 
               className='flex flex-col bg-black justify-center h-screen'
               onMouseMove={showControls}
               onClick={showControls}
-              >
-          <video muted={Mute} 
-              ref={videoRef}
-              className="h-full"
-              onLoadedMetadata={handleLoadedMetadata}
-              onClick={handleplay}>
-            <source src={url} type="video/mp4"/>
-            Your browser does not support the video tag.
-          </video>      
-          <div className={`fixed bottom-0 w-full`}>
-            {/* black shadow on top controls */}
-            <div className={`fixed -top-24 w-screen bg-black h-48 blur-2xl bg-opacity-70  duration-700 ease-in-out ${ControlsVisible?"translate-y-0":"-translate-y-40"}`}></div>
-            <button className={`text-2xl w-full px-48 py-12 fixed top-0 left-0  duration-700 ease-in-out ${ControlsVisible?"translate-y-0":"-translate-y-40"}`} 
-                  onClick={()=>{ router.push('/')}}>
-                    <div className='flex items-center'>
-                      <ArrowLeft/><p className='text-xl mx-4'>Title</p>
+              >     
+          <Suspense fallback={<div>Loading...</div>}>     
+              <video muted={Mute} 
+                  ref={videoRef}
+                  className="h-full"
+                  onLoadedMetadata={handleLoadedMetadata}
+                  onClick={handleplay}
+                  // autoPlay
+                  >
+                <source src={url} type="video/mp4"/>
+                Your browser does not support the video tag.
+              </video>      
+              <div className={`fixed bottom-0 w-full`}>
+                {/* black shadow on top controls */}
+                <div className={`fixed -top-24 w-screen bg-black h-48 blur-2xl bg-opacity-70  duration-700 ease-in-out ${ControlsVisible?"translate-y-0":"-translate-y-40"}`}></div>
+                <button className={`text-2xl w-full px-48 py-12 fixed top-0 left-0  duration-700 ease-in-out ${ControlsVisible?"translate-y-0":"-translate-y-40"}`} 
+                      onClick={()=>{ router.push('/')}}>
+                        <div className='flex items-center'>
+                          <ArrowLeft/><p className='text-xl mx-4'>Title</p>
+                        </div>
+                </button>
+                {/* center icons */}
+                <div className={`text-8xl fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-scale duration-500 ${CenterIcon ? 'scale-100' : 'scale-0'}`} onClick={handleplay}>{CenterTag}</div>
+                {/* black shadow on controls */}
+                <div className={`fixed -bottom-24 w-screen bg-black h-48 blur-2xl bg-opacity-70  duration-700 ease-in-out ${ControlsVisible?"translate-y-0":"translate-y-40"}`}></div>
+                
+                <div className={`py-4 mx-48 duration-700 ease-in-out ${ControlsVisible?"translate-y-0":"translate-y-40"}`}>  
+                  <div className="mb-2">
+                    <input className='w-full h-1 cursor-pointer' 
+                          type="range" 
+                          value={SeekVal} 
+                          onChange={handleseek} />
+                    <p>{(CurrentTime==="NaN:NaN:NaN"||Duration==="NaN:NaN:NaN")?'':`${CurrentTime} / ${Duration}`}</p>
+                  </div>
+                  <div className='flex  justify-between'>
+                    <div className='flex justify-between w-1/3'>
+                      <button className='text-2xl' onClick={handlereverse}><ChevronDoubleLeft/></button>
+                      <button className='text-6xl' onClick={handleplay}>{Play?<PlayFill/>:<PauseFill/>}</button>
+                      <button className='text-2xl' onClick={handleforward}><ChevronDoubleRight/></button>
+                      <div className='flex items-center' onMouseEnter={()=>setShowVolbar(true)} onMouseLeave={()=>setShowVolbar(false)}>
+                        <button className='text-2xl'onClick={()=>{setMute(!Mute);  Mute?setCenterTag(<VolumeUpFill/>):setCenterTag(<VolumeMute/>)}}>{Mute?<VolumeMute/>:<VolumeUpFill/>}</button>
+                        <input className={`ml-2 w-20 h-0.5 cursor-pointer transition-scale duration-700 ease-in-out origin-left ${ShowVolbar?"scale-x-100":"scale-x-0"}`} 
+                            type="range" 
+                            value={Mute?0:Vol} 
+                            onChange={handlevolume} 
+                            min={0} 
+                            max={1} 
+                            step={0.01} />
+                      </div>  
                     </div>
-            </button>
-            {/* center icons */}
-            <div className={`text-8xl fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-scale duration-500 ${CenterIcon ? 'scale-100' : 'scale-0'}`} onClick={handleplay}>{CenterTag}</div>
-            {/* black shadow on controls */}
-            <div className={`fixed -bottom-24 w-screen bg-black h-48 blur-2xl bg-opacity-70  duration-700 ease-in-out ${ControlsVisible?"translate-y-0":"translate-y-40"}`}></div>
-            
-            <div className={`py-4 mx-48 duration-700 ease-in-out ${ControlsVisible?"translate-y-0":"translate-y-40"}`}>  
-              <div className="mb-2">
-                <input className='w-full h-1 cursor-pointer' 
-                      type="range" 
-                      value={SeekVal} 
-                      onChange={handleseek} />
-                <p>{(CurrentTime==="NaN:NaN:NaN"||Duration==="NaN:NaN:NaN")?'':`${CurrentTime} / ${Duration}`}</p>
-              </div>
-              <div className='flex  justify-between'>
-                <div className='flex justify-between w-1/3'>
-                  <button className='text-2xl' onClick={handlereverse}><ChevronDoubleLeft/></button>
-                  <button className='text-6xl' onClick={handleplay}>{Play?<PlayFill/>:<PauseFill/>}</button>
-                  <button className='text-2xl' onClick={handleforward}><ChevronDoubleRight/></button>
-                  <div className='flex items-center' onMouseEnter={()=>setShowVolbar(true)} onMouseLeave={()=>setShowVolbar(false)}>
-                    <button className='text-2xl'onClick={()=>{setMute(!Mute);  Mute?setCenterTag(<VolumeUpFill/>):setCenterTag(<VolumeMute/>)}}>{Mute?<VolumeMute/>:<VolumeUpFill/>}</button>
-                    <input className={`ml-2 w-20 h-0.5 cursor-pointer transition-scale duration-700 ease-in-out origin-left ${ShowVolbar?"scale-x-100":"scale-x-0"}`} 
-                        type="range" 
-                        value={Mute?0:Vol} 
-                        onChange={handlevolume} 
-                        min={0} 
-                        max={1} 
-                        step={0.01} />
+                    <div className='flex items-center'>
+                      <div className='flex items-center'>
+                        <select defaultValue={PlaybackRate} className='text-sm s outline-0 bg-transparent mx-14 py-1 cursor-pointer' onChange={handlePlaybackRate}>
+                            {playbackOptions.map((option) => (
+                              <option className='bg-neutral-800' key={option} value={option}>
+                                {option}x
+                              </option>
+                            ))}
+                        </select>
+                        <button className='text-xl'onClick={handlefullscreen}>{fullscreen?<FullscreenExit/>:<Fullscreen/>}</button> 
+                      </div>
+                    </div>
                   </div>  
                 </div>
-                <div className='flex items-center'>
-                  <div className='flex items-center'>
-                    <select defaultValue={PlaybackRate} className='text-sm s outline-0 bg-transparent mx-14 py-1 cursor-pointer' onChange={handlePlaybackRate}>
-                        {playbackOptions.map((option) => (
-                          <option className='bg-neutral-800' key={option} value={option}>
-                            {option}x
-                          </option>
-                        ))}
-                    </select>
-                    <button className='text-xl'onClick={handlefullscreen}>{fullscreen?<FullscreenExit/>:<Fullscreen/>}</button> 
-                  </div>
-                </div>
-              </div>  
-            </div>
-          </div>  
+              </div> 
+          </Suspense> 
         </div>   
   );
 };
